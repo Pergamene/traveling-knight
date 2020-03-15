1bc2359b7b89a89be9c2f77e9ae7e897451cebf2
Pergamene <smartz373@gmail.com>
2020-03-15 15:03:10 -0600

    Updates

---
+++ precache-manifest.6476290dfee87123bea19cb0f8ee2c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984e6c009a7544cb378602ce190c0c51",
+    "url": "/traveling-knight/index.html"
+  },
+  {
+    "revision": "d8bccec07b2fec93b401",
+    "url": "/traveling-knight/static/js/2.bfdb5793.chunk.js"
+  },
+  {
+    "revision": "6a06bded57bffdabfb3d4b28baf42b9e",
+    "url": "/traveling-knight/static/js/2.bfdb5793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f67d58df997f8439378",
+    "url": "/traveling-knight/static/js/main.d17d3d7c.chunk.js"
+  },
+  {
+    "revision": "35c1cd56e6f34c8fab27",
+    "url": "/traveling-knight/static/js/runtime-main.d784c346.js"
+  }
+]);
